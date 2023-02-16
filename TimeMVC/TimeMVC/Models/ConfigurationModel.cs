f07ff5587507a3734d8dccd95d02926ba549da23
Nora Gu <catusgn@qq.com>
2023-02-16 16:58:32 +0800

    upload

---
+++ TimeMVC/TimeMVC/Models/ConfigurationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeMVC.Models
+{
+    public class ConfigurationModel
+    {
+        public string DefaultConnection { get; set; }
+    }
+}

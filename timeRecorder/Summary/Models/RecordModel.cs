1860c7cadc3ba95e668ec0c2128d2def54d23674
Nora Gu <catusgn@qq.com>
2023-11-07 16:41:42 +0800

    directly delete task of none type

--- timeRecorder/Summary/Models/RecordModel.cs
+++ timeRecorder/Summary/Models/RecordModel.cs
@@ -653,7 +653,7 @@ namespace Summary.Models
         private async Task CheckAndDeleteToDo(ToDoObj objTobeDeleted)
         {
             var objs = SQLCommands.GetTimeObjsByName(objTobeDeleted.Note);
-            if (objs.Count()==0||objTobeDeleted.Type=="none")
+            if (objs.Count()==0||objTobeDeleted.Type=="none"||objTobeDeleted.Type=="")
             {
                 await SQLCommands.DeleteTodo(objTobeDeleted);
             }

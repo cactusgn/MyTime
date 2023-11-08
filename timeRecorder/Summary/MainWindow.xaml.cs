fd38c7124ea4b5d64a1f94e52085aee2f31b0612
Nora Gu <catusgn@qq.com>
2023-11-08 14:37:35 +0800

    update multiple monitors

--- timeRecorder/Summary/MainWindow.xaml.cs
+++ timeRecorder/Summary/MainWindow.xaml.cs
@@ -31,11 +31,12 @@ namespace Summary
     /// </summary>
     public partial class MainWindow : Window
     {
+    
         public MainWindow(MainModel mainModel)
         {
             InitializeComponent();
             this.DataContext = mainModel;
-            LocationChanged += MainWindow_LocationChanged;
+           
 
             this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
             this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
@@ -46,14 +47,16 @@ namespace Summary
             btnMax.Click += (s, e) => {
                 if (this.WindowState == WindowState.Maximized)
                 {
-                    this.WindowState = WindowState.Normal;
+                    Restore(this);
+                    //this.WindowState = WindowState.Normal;
                     btnMaxIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowMaximize;
                 }
                 else
                 {
-                    this.WindowState = WindowState.Maximized;
+                   Maximize(this);
+                    //this.WindowState = WindowState.Maximized;
                     btnMaxIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowRestore;
-                  
+                    
                 }
             };
             btnClose.Click += (s, e) => { this.Close(); minimizeWindow.Close(); };
@@ -95,12 +98,12 @@ namespace Summary
             {
                 if (this.WindowState == WindowState.Normal)
                 {
-                    this.WindowState = WindowState.Maximized;
+                    Maximize(this);
                     btnMaxIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowRestore;
                 }
                 else
                 {
-                    this.WindowState = WindowState.Normal;
+                    Restore(this);
                     btnMaxIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowMaximize;
                    
                 }
@@ -108,24 +111,110 @@ namespace Summary
             };
             
         }
-        private void MainWindow_LocationChanged(object sender, EventArgs e)
-        {
-            UpdateScreenInfo(this);
+        // Rectangle (used by MONITORINFOEX)
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
         }
-        private void UpdateScreenInfo(Window window)
-        {
-            var hwndSource = (HwndSource)PresentationSource.FromVisual(window);
+
+        // Monitor information (used by GetMonitorInfo())
+        [StructLayout(LayoutKind.Sequential)]
+        public class MonitorInfoEx {
+            public int cbSize;
+            public Rect rcMonitor; // Total area
+            public Rect rcWork; // Working area
+            public int dwFlags;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x20)]
+            public char[] szDevice;
+        }
+                // To get a handle to the specified monitor
+        [DllImport("user32.dll")]
+        private static extern IntPtr MonitorFromWindow(IntPtr hwnd, int dwFlags);
+
+        // To get the working area of the specified monitor
+        [DllImport("user32.dll")]
+        private static extern bool GetMonitorInfo(HandleRef hmonitor, [In, Out] MonitorInfoEx monitorInfo);
+
+        private static MonitorInfoEx GetMonitorInfo(Window window, IntPtr monitorPtr) {
+            var monitorInfo = new MonitorInfoEx();
+
+            monitorInfo.cbSize = Marshal.SizeOf(monitorInfo);
+            GetMonitorInfo(new HandleRef(window, monitorPtr), monitorInfo);
+
+            return monitorInfo;
+        }
+
+        private static void Minimize(Window window) {
+             if(window == null) {
+                 return;
+             }
+
+             window.WindowState = WindowState.Minimized;
+         }
+
+         private void Restore(Window window) {
+            if(window == null) {
+                return;
+            }
+
+            window.WindowState = WindowState.Normal;
+            window.ResizeMode = ResizeMode.CanResizeWithGrip;
+
+             var hwndSource = (HwndSource)PresentationSource.FromVisual(window);
             if (hwndSource is null)
             {
                 return;
             }
             var hWnd = hwndSource.Handle;
-            //var screen = System.Windows.Forms.Screen.FromHandle(hWnd).Bounds;
-            //var screen = System.Windows.Forms.Screen.FromHandle(hWnd);
             var handle = new System.Windows.Interop.WindowInteropHelper(window).Handle;
             var screen = System.Windows.Forms.Screen.FromHandle(handle);
-            this.MaxHeight = screen.WorkingArea.Height + 8;
-            this.MaxWidth = screen.WorkingArea.Width + 8;
+
+            this.Top = this.Top + (screen.WorkingArea.Height - this.Height)/2;
+            this.Left = this.Left + (screen.WorkingArea.Width - this.Width)/2;
+        }
+
+        private static void Maximize(Window window) {
+            window.ResizeMode = ResizeMode.NoResize;
+
+            // Get handle for nearest monitor to this window
+            var wih = new WindowInteropHelper(window);
+
+            // Nearest monitor to window
+            const int MONITOR_DEFAULTTONEAREST = 2;
+            var hMonitor = MonitorFromWindow(wih.Handle, MONITOR_DEFAULTTONEAREST);
+
+            // Get monitor info
+            var monitorInfo = GetMonitorInfo(window, hMonitor);
+
+            // Create working area dimensions, converted to DPI-independent values
+            var source = HwndSource.FromHwnd(wih.Handle);
+
+            if(source?.CompositionTarget == null) {
+                return;
+            }
+
+            var matrix = source.CompositionTarget.TransformFromDevice;
+            var workingArea = monitorInfo.rcWork;
+
+            var dpiIndependentSize =
+                matrix.Transform(
+                    new Point(workingArea.Right - workingArea.Left,
+                                workingArea.Bottom - workingArea.Top));
+
+
+
+            // Maximize the window to the device-independent working area ie
+            // the area without the taskbar.
+            window.Top = workingArea.Top;
+            window.Left = workingArea.Left;
+
+            window.MaxWidth = dpiIndependentSize.X;
+            window.MaxHeight = dpiIndependentSize.Y;
+
+            window.WindowState = WindowState.Maximized;
         }
     }
 }
